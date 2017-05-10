6a4ddcace985bfa352f8a898ae0abc1b62a6105f
Luka Kovacevic <luka.kovacevic.jr@gmail.com>
2017-05-10 18:36:27 +0200

    -- added packages and configuration for tests

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,50 +1,49 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
-class AppKernel extends Kernel
-{
-    public function registerBundles()
-    {
-        $bundles = [
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
-        ];
+class AppKernel extends Kernel {
+	public function registerBundles() {
+		$bundles = [
+			new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+			new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+			new Symfony\Bundle\TwigBundle\TwigBundle(),
+			new Symfony\Bundle\MonologBundle\MonologBundle(),
+			new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+			new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+			new AppBundle\AppBundle(),
+		];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
+		if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+			$bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+			$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+			$bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+			$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+			$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
 
-        return $bundles;
-    }
+			if ('test' === $this->getEnvironment()) {
+				$bundles[] = new Liip\FunctionalTestBundle\LiipFunctionalTestBundle();
+			}
+		}
 
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
+		return $bundles;
+	}
 
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
+	public function getRootDir() {
+		return __DIR__;
+	}
 
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
-    }
+	public function getCacheDir() {
+		return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
+	}
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
-    }
+	public function getLogDir() {
+		return dirname(__DIR__) . '/var/logs';
+	}
+
+	public function registerContainerConfiguration(LoaderInterface $loader) {
+		$loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+	}
 }
