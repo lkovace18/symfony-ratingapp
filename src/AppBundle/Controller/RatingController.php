66433324870f82fed0f578498438a555bdae5c37
Luka Kovacevic <luka.kovacevic.jr@gmail.com>
2017-05-10 20:00:20 +0200

    -- added rest bundles, added api documentation

--- src/AppBundle/Controller/RatingController.php
+++ src/AppBundle/Controller/RatingController.php
@@ -2,23 +2,73 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpFoundation\Request;
 
-class RatingController extends Controller {
+class RatingController extends FOSRestController {
 
 	/**
-	 * @Route("/rating/votes", name="rating_votes")
+	 * GET Specific Uri Rating
+	 *
+	 * @ApiDoc(
+	 *  resource=true,
+	 *  description="Specific Uri Rating",
+	 *  requirements={
+	 *      {
+	 *          "name"="uri",
+	 *          "dataType"="string",
+	 *          "requirement"="",
+	 *          "description"="Uri of site"
+	 *      }
+	 *  },
+	 *  statusCodes={
+	 *    200="Returned when successful",
+	 *  }
+	 * )
+	 *
+	 * @Get("/rating/votes", name="rating_list_votes")
 	 */
-	public function listAction(Request $request) {
+	public function getUriRatingAction(Request $request) {
 		//
 	}
 
 	/**
-	 * @Route("/rating/votes", name="rating_vote")
+	 * POST Vote for specific Uri.
+	 *
+	 * @ApiDoc(
+	 *  resource=true,
+	 *  description="Vote for specific Uri.",
+	 *  requirements={
+	 *    {
+	 *        "name"="visitor_id",
+	 *        "dataType"="string",
+	 *        "requirement"="",
+	 *        "description"="Uri of site"
+	 *    },
+	 *    {
+	 *        "name"="uri",
+	 *        "dataType"="string",
+	 *        "requirement"="",
+	 *        "description"="Uri of site"
+	 *    },
+	 *    {
+	 *       "name"="rating",
+	 *       "dataType"="integer",
+	 *       "requirement"="",
+	 *       "description"="Rateing for site can be 1-10"
+	 *     }
+	 *  },
+	 *  statusCodes={
+	 *    200="Returned when successful",
+	 *  } *
+	 * )
+	 *
+	 * @Post("/rating/votes", name="rating_vote")
 	 */
-	public function voteAction(Request $request) {
+	public function voteForUriAction(Request $request) {
 		//
 	}
 
