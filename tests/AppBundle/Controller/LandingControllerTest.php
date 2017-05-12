f5df93f5840d2c434e6a2f08a109012bb1c300e8
lkovace18 <luka.kovacevic.jr@gmail.com>
2017-05-12 21:02:42 +0000

    Apply fixes from StyleCI

--- tests/AppBundle/Controller/LandingControllerTest.php
+++ tests/AppBundle/Controller/LandingControllerTest.php
@@ -4,47 +4,50 @@ namespace Tests\AppBundle\Controller;
 
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
-class LandingControllerTest extends WebTestCase {
+class LandingControllerTest extends WebTestCase
+{
+    /** @test */
+    public function it_displays_langing_page_correctly()
+    {
+        $client = $this->makeClient();
 
-	/** @test */
-	function it_displays_langing_page_correctly() {
-		$client = $this->makeClient();
+        $client->request('GET', '/');
+        $this->assertStatusCode(200, $client);
 
-		$client->request('GET', '/');
-		$this->assertStatusCode(200, $client);
+        $this->assertContains(
+            'RateMe App',
+            $client->getResponse()->getContent()
+        );
+    }
 
-		$this->assertContains(
-			'RateMe App',
-			$client->getResponse()->getContent()
-		);
-	}
+    /** @test */
+    public function it_displays_js_widget()
+    {
+        $client = $this->makeClient();
 
-	/** @test */
-	function it_displays_js_widget() {
-		$client = $this->makeClient();
+        $client->request('GET', '/');
+        $this->assertStatusCode(200, $client);
 
-		$client->request('GET', '/');
-		$this->assertStatusCode(200, $client);
+        $this->assertContains(
+            'Try it, rate me ;)',
+            $client->getResponse()->getContent()
+        );
+    }
 
-		$this->assertContains(
-			'Try it, rate me ;)',
-			$client->getResponse()->getContent()
-		);
-	}
+    /** @test */
+    public function it_displays_api_documentation_when_clicked_on_link()
+    {
+        $client = $this->makeClient();
 
-	/** @test */
-	function it_displays_api_documentation_when_clicked_on_link() {
-		$client = $this->makeClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertStatusCode(200, $client);
+        $link = $crawler->selectLink('API Documentation')->link();
 
-		$crawler = $client->request('GET', '/');
-		$this->assertStatusCode(200, $client);
-		$link = $crawler->selectLink('API Documentation')->link();
+        $client->request('GET', $link->getUri());
 
-		$client->request('GET', $link->getUri());
-
-		$this->assertContains(
-			'API documentation',
-			$client->getResponse()->getContent()
-		);
-	}
+        $this->assertContains(
+            'API documentation',
+            $client->getResponse()->getContent()
+        );
+    }
 }
